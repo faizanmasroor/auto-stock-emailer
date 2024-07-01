a336e386528f9d191f6e2709b13b2d53f14f11f6
Faizan Masroor (Post Oak) <faizanmasroor01@gmail.com>
2024-07-01 17:23:49 -0500

    Begin development

--- deliver-graphs.ps1
+++ deliver-graphs.ps1
@@ -1,4 +1,16 @@
-$FileName = 'test_script.py'
-$Command = $PSScriptRoot+'\'+$FileName
+#    $FileName = 'test_script.py'
+#    $Command = $PSScriptRoot+'\'+$FileName
+#
+#    Start-Process -FilePath $Command
 
-Start-Process -FilePath $Command
+$CSVFilePath = Join-Path -Path $PSScriptRoot -ChildPath "email_list.csv"
+$CSVTable = Import-Csv -Path $CSVFilePath
+$Emails = $CSVTable.email
+$TickerLists = $CSVTable.tickers
+
+for ($i = 0; $i -lt 3; $i++)
+{
+    $Email = $Emails[$i]
+    $TickerList = $TickerLists[$i]
+    $Tickers=$TickerList.split(",")
+}
